69b5e24f7d637d1a96317756b67df3c669077ab3
Frankie Cho <frankiechoht@gmail.com>
2023-06-13 15:18:52 +1000

    discount rate and mcmc draws

--- code/sensitivity_analysis.jl
+++ code/sensitivity_analysis.jl
@@ -24,6 +24,13 @@ cost_df = CSV.read("data/spatial_predictions_10yr.csv", DataFrame);
 kitl_index_full = CSV.read("data/kitl_prop_climate.csv", DataFrame);
 stratified_samples = CSV.read("data/stratified_sample.csv", DataFrame);
 climateProjList = ["CCCMA_R1", "CCCMA_R2", "CCCMA_R3", "CSIRO_R1", "CSIRO_R2", "CSIRO_R3", "ECHAM_R1", "ECHAM_R2", "ECHAM_R3", "MIROC_R1", "MIROC_R2", "MIROC_R3"];
+discount_rate = 0.02;
+model_matrix_x = [convert(Matrix{Float64}, CSV.read("data/model_matrix/Model_MatrixX.10yr_$i.csv", DataFrame, header=false)) for i in 1:10];
+model_matrix_y = [convert(Matrix{Float64}, CSV.read("data/model_matrix/Model_MatrixY.10yr_$i.csv", DataFrame, header=false)) for i in 1:10];
+model_matrix_z = [convert(Matrix{Float64}, CSV.read("data/model_matrix/Model_MatrixZ.10yr_$i.csv", DataFrame, header=false)) for i in 1:10];
+coefs_x = [convert(Matrix{Float64}, CSV.read("data/model_matrix/CoefsX.10yr_$i.csv", DataFrame, header=false)) for i in 1:10];
+coefs_y = [convert(Matrix{Float64}, CSV.read("data/model_matrix/CoefsY.10yr_$i.csv", DataFrame, header=false)) for i in 1:10];
+coefs_z = [convert(Matrix{Float64}, CSV.read("data/model_matrix/CoefsZ.10yr_$i.csv", DataFrame, header=false)) for i in 1:10];
 
 # Constants
 R = 100; # Total number of uncertainty realisations
@@ -40,16 +47,18 @@ mutable struct Solution
     metric::AbstractArray
 end
 
-function cost_to_ts(cost::AbstractVector, area::AbstractVector, adopt::AbstractVector, idx_before::AbstractVector=1:30, idx_after::AbstractVector=31:60, inflation_rate::AbstractFloat=0.02)
+# Cost to time series
+function cost_to_ts(cost::AbstractVector, area::AbstractVector, prop::AbstractVector, idx_before::AbstractVector=1:30, idx_after::AbstractVector=31:60, inflation_rate::AbstractFloat= -discount_rate)
     delta = (1 + inflation_rate) .^ (cat(idx_before, idx_after, dims=1))
     delta_before = sum(delta[idx_before])
     delta_after = sum(delta[idx_after])
-    cost_ts = (cost .* 1000 .* area .* adopt ./ 10) # Cost per-year
+    cost_ts = (cost .* 1000 .* area .* prop ./ 10) # Cost per-year: cost per ha * area * proportion / 10
     cost_before = cost_ts .* delta_before # Cost start at 2020
     cost_after = cost_ts .* delta_after
     return (cost_before, cost_after)
 end
 
+# Subset data
 function subset_data(cost_full, kitl_index_full, subset_id::Vector{Int64})
     cost_subset = filter(row -> row.NewPropID ∈ subset_id, cost_full)
     kitl_subset = filter(row -> row.NewPropID ∈ subset_id, kitl_index_full)
@@ -58,6 +67,7 @@ function subset_data(cost_full, kitl_index_full, subset_id::Vector{Int64})
     return (cost_subset, kitl_subset)
 end
 
+# Metrics to input
 function metric_to_input(metric::DataFrame, area::AbstractVector, tt::Integer, kt::AbstractFloat)
     # Formats metrics to input
     t_symbols = [:t0, :t1, :t2, :t3, :t4, :t5, :t6, :t7]
@@ -79,6 +89,7 @@ function metric_to_input(metric::DataFrame, area::AbstractVector, tt::Integer, k
     return (M₁, M₂)
 end
 
+# Realisation
 function realisation_sample(cost_subset::DataFrame, kitl_subset::DataFrame, tt::Integer=4, kt::AbstractFloat=0.25)
     (M₁, M₂) = metric_to_input(kitl_subset, vec(cost_subset.AREA), tt, kt)
     MeanAdopt = cost_subset.MeanAdopt
@@ -89,7 +100,7 @@ function realisation_sample(cost_subset::DataFrame, kitl_subset::DataFrame, tt::
     SDWTA = cost_subset.SDWTA
     adopt_binary = (rand.(Normal.(MeanAdopt, SDAdopt))) .> rand(length(MeanAdopt))
     prop = (rand.(Normal.(MeanProp, SDProp))) .* adopt_binary
-    (C₁, C₂) = cost_to_ts(MeanWTA + SDWTA .* randn(length(SDWTA)), cost_subset.AREA, cost_subset.MeanAdopt, 1:(10*(tt-1)), (1+10*(tt-1)):60, 0.02)
+    (C₁, C₂) = cost_to_ts(MeanWTA + SDWTA .* randn(length(SDWTA)), cost_subset.AREA, prop_binary, 1:(10*(tt-1)), (1+10*(tt-1)):60, 0.02)
     sample_C₁ = C₁ .* prop
     sample_C₂ = C₂ .* prop
     sample_M₁ = M₁ .* prop
@@ -98,19 +109,68 @@ function realisation_sample(cost_subset::DataFrame, kitl_subset::DataFrame, tt::
     return Realisation(sample_C₁, sample_C₂, sample_M₁, sample_M₂, area, adopt_binary)
 end
 
+# Realisation from MCMC sampling draws, with cost_subset constructed from fcn_get_predictions
+function fcn_realisation_sample_draws(cost_subset::DataFrame, kitl_subset::DataFrame, tt::Integer=4, kt::AbstractFloat=0.25)
+    (M₁, M₂) = metric_to_input(kitl_subset, area, tt, kt)
+    adopt_binary = adopt .< rand(length(cost_subset.Adopt))
+    prop_binary = prop .* adopt_binary
+    (C₁, C₂) = cost_to_ts(cost_subset.WTA, cost_subset.AREA, prop_binary, 1:(10*(tt-1)), (1+10*(tt-1)):60, -discount_rate)
+    sample_C₁ = C₁ .* prop_binary
+    sample_C₂ = C₂ .* prop_binary
+    sample_M₁ = M₁ .* prop_binary
+    sample_M₂ = M₂ .* prop_binary
+    area = vec(cost_subset.AREA) .* prop_binary
+    return Realisation(sample_C₁, sample_C₂, sample_M₁, sample_M₂, area, adopt_binary)
+end
+
+function fcn_get_predictions()
+    # Get cost, prop and adoption predictions from model matrix and coefficient multiplication
+    imputation_idx = rand(1:10)
+    draw_idx = rand(1:10000)
+
+    # Get model matrix and coefficients
+    model_matrix_x_idx = model_matrix_x[imputation_idx]
+    model_matrix_y_idx = model_matrix_y[imputation_idx]
+    model_matrix_z_idx = model_matrix_z[imputation_idx]
+    coefs_x_idx = coefs_x[imputation_idx][draw_idx,:]
+    coefs_y_idx = coefs_y[imputation_idx][draw_idx,:]
+    coefs_z_idx = coefs_z[imputation_idx][draw_idx,:]
+
+    # Get predictions
+    pred_x = model_matrix_x_idx * coefs_x_idx
+    pred_y = model_matrix_y_idx * coefs_y_idx
+    pred_z = model_matrix_z_idx * coefs_z_idx
+
+    # Logit transform adopt and prop 
+    pred_x = exp(pred_x) ./ (1 + exp(pred_x))
+    pred_z = exp(pred_z) ./ (1 + exp(pred_z))
+
+    # Collate into DataFrame
+    df = DataFrame(WTA = pred_y, Prop = pred_z, Adopt = pred_x, AREA = cost_df.AREA)
+
+    # Output predictions in a dataframe
+    return df
+end
+
+function fcn_subset_realisation_sample_draw(kitl_index_full::DataFrame, subset_id::AbstractVector, tt::Integer=4, kt::AbstractFloat=0.25)
+    cost_df = fcn_get_predictions();
+    (cost_subset, kitl_subset) = subset_data(cost_df, kitl_index_full, subset_id)
+    realisation = realisation_sample(cost_subset, kitl_subset, tt, kt)
+    return realisation;
+end
+
 function fcn_tidy_two_stage_solution_sum(solution, prop_id)
     out = hcat(prop_id, solution.x, sum(solution.y, dims=2), sum(solution.w, dims=2))
     colnames = vcat(["NewPropID"; "x"; "sum_y"; "sum_w"])
     return DataFrame(out, colnames)
 end
 
-function fcn_run_optim(cost_df::DataFrame, kitl_index_full::DataFrame, stratified_samples::DataFrame, out_dir::AbstractString, sp::Integer, tt::Integer, kt::AbstractFloat, ns::Integer, baseline_conditions=false)
+function fcn_run_optim(kitl_index_full::DataFrame, stratified_samples::DataFrame, out_dir::AbstractString, sp::Integer, tt::Integer, kt::AbstractFloat, ns::Integer, baseline_conditions=false)
     Random.seed!(54815861) # Ensure all realisation samples are the same
     # Subset of properties
     run_string = "run_sp-$(sp)_tt-$(tt)_kt-$(kt)_ns-$(ns)_r-$(rr)"
     subset_id = vec(stratified_samples[:, sp])
-    (cost_subset, kitl_subset) = subset_data(cost_df, kitl_index_full, subset_id)
-    realisations = [realisation_sample(cost_subset, kitl_subset, tt, kt) for r in 1:R]
+    realisations = [fcn_subset_realisation_sample_draw(kitl_index_full, subset_id, tt, kt) for r in 1:R]
     realisation_areas = hcat([realisations[r].area for r in 1:R]...)
     realisation_areas_df = DataFrame(realisation_areas, :auto)
     CSV.write("$(out_dir)/area_$(run_string).csv", realisation_areas_df)
@@ -198,21 +258,21 @@ kt_vec = [0.1, 0.125, 0.15, 0.175, 0.2, 0.225, 0.25, 0.275, 0.3]
 ns = 12; # 1:12
 ns_vec = 1:12
 
-baseline = fcn_run_optim(cost_df, kitl_index_full, stratified_samples, "results/mc_sim", sp, tt, kt, ns, true)
+baseline = fcn_run_optim(kitl_index_full, stratified_samples, "results/mc_sim", sp, tt, kt, ns, true)
 
-robust = fcn_run_optim(cost_df, kitl_index_full, stratified_samples, "results/mc_sim", sp, tt, kt, ns, false)
+robust = fcn_run_optim(kitl_index_full, stratified_samples, "results/mc_sim", sp, tt, kt, ns, false)
 
 # Run across all ns, number of scenarios in resolved uncertainty
-map((ns_i) -> fcn_run_optim(cost_df, kitl_index_full, stratified_samples, "results/mc_sim", sp, tt, kt, ns_i), ns_vec)
+map((ns_i) -> fcn_run_optim(kitl_index_full, stratified_samples, "results/mc_sim", sp, tt, kt, ns_i), ns_vec)
 
 # Run across different time periods for tt
-map((tt_i) -> fcn_run_optim(cost_df, kitl_index_full, stratified_samples, "results/mc_sim", sp, tt_i, kt, 1), tt_vec)
-map((tt_i) -> fcn_run_optim(cost_df, kitl_index_full, stratified_samples, "results/mc_sim", sp, tt_i, kt, ns), tt_vec)
+map((tt_i) -> fcn_run_optim(kitl_index_full, stratified_samples, "results/mc_sim", sp, tt_i, kt, 1), tt_vec)
+map((tt_i) -> fcn_run_optim(kitl_index_full, stratified_samples, "results/mc_sim", sp, tt_i, kt, ns), tt_vec)
 
 # Run across all kt
-map((kt_i) -> fcn_run_optim(cost_df, kitl_index_full, stratified_samples, "results/mc_sim", sp, tt, kt_i, 1), kt_vec)
-map((kt_i) -> fcn_run_optim(cost_df, kitl_index_full, stratified_samples, "results/mc_sim", sp, tt, kt_i, ns), kt_vec)
+map((kt_i) -> fcn_run_optim(kitl_index_full, stratified_samples, "results/mc_sim", sp, tt, kt_i, 1), kt_vec)
+map((kt_i) -> fcn_run_optim(kitl_index_full, stratified_samples, "results/mc_sim", sp, tt, kt_i, ns), kt_vec)
 
 # Run across different sampled properties
-map((sp_i) -> fcn_run_optim(cost_df, kitl_index_full, stratified_samples, "results/mc_sim", sp_i, tt, kt, 1), sp_vec)
-map((sp_i) -> fcn_run_optim(cost_df, kitl_index_full, stratified_samples, "results/mc_sim", sp_i, tt, kt, ns), sp_vec)
+map((sp_i) -> fcn_run_optim(kitl_index_full, stratified_samples, "results/mc_sim", sp_i, tt, kt, 1), sp_vec)
+map((sp_i) -> fcn_run_optim(kitl_index_full, stratified_samples, "results/mc_sim", sp_i, tt, kt, ns), sp_vec)
